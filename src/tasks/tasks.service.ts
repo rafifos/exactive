82d04f63d63a82b46a7bfd77058b22057ec9c9e9
Rafael Julio <hello.rjulio@gmail.com>
2020-01-15 15:49:21 -0300

    feature(tasks): throw error if try to delete a non-existant task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -59,6 +59,8 @@ export class TasksService {
   }
 
   deleteTask(id: string): void {
-    this.tasks = this.tasks.filter(task => task.id !== id);
+    const found = this.getTaskById(id);
+
+    this.tasks = this.tasks.filter(task => task.id !== found.id);
   }
 }
