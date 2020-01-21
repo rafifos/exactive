f048654f65381b9e72d326dfeb936de4fead30b6
Rafael Julio <hello.rjulio@gmail.com>
2020-01-21 14:58:31 -0300

    feature(auth): implement JWT token generation on sign-in

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserRepository } from './user.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    JwtModule.register({
+      secret: '923A45FB-D987-4B7B-94A5-B364D1A116DB',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    TypeOrmModule.forFeature([UserRepository]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
