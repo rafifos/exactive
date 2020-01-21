429dc17a57b65bbacda17938283bd63eb2caa09f
Rafael Julio <hello.rjulio@gmail.com>
2020-01-21 14:41:15 -0300

    feature(auth): implement user validation

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,3 +1,4 @@
+import * as bcrypt from 'bcrypt';
 import {
   BaseEntity,
   Column,
@@ -20,4 +21,9 @@ export class User extends BaseEntity {
 
   @Column()
   password: string;
+
+  async validatePassword(password: string): Promise<boolean> {
+    const hash = await bcrypt.hash(password, this.salt);
+    return hash === this.password;
+  }
 }
