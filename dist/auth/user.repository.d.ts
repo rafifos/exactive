d5b191068c86b9d05514df853bde791c4dd400fd
Rafael Julio <hello.rjulio@gmail.com>
2020-01-23 17:35:45 -0300

    refactor: delete dist/

--- dist/auth/user.repository.d.ts
+++
@@ -1,9 +0,0 @@
-import { Repository } from 'typeorm';
-import { AuthCredentialsDto } from './dto/auth-credentials.dto';
-import { User } from './user.entity';
-export declare class UserRepository extends Repository<User> {
-    private logger;
-    signUp(authCredentialsDto: AuthCredentialsDto): Promise<void>;
-    validateUserPassword(authCredentialsDto: AuthCredentialsDto): Promise<string>;
-    private hashPassword;
-}
