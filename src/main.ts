bc41efe12b966347fcc7e61aaf3fe17771f210ba
Rafael Julio <hello.rjulio@gmail.com>
2020-01-23 13:48:54 -0300

    feature(tasks): log bootstrap actions

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const applicationPort = 3000;
+
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.listen(applicationPort);
+  logger.log(`Application listening on port ${applicationPort}`);
 }
 bootstrap();
