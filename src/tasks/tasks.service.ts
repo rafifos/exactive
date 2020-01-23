54158e231eb1de424867216973290b69348f5fd4
Rafael Julio <hello.rjulio@gmail.com>
2020-01-23 13:19:40 -0300

    feature(tasks): implement user authorization on getTaskById

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -18,8 +18,10 @@ export class TasksService {
     return this.taskRepository.getTasks(filterDto, user);
   }
 
-  async getTaskById(id: number): Promise<Task> {
-    const found = await this.taskRepository.findOne(id);
+  async getTaskById(id: number, user: User): Promise<Task> {
+    const found = await this.taskRepository.findOne({
+      where: { id, userId: user.id },
+    });
 
     if (!found) {
       throw new NotFoundException(
