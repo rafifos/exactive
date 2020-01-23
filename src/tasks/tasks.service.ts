241ea5586539fbd2d1eed6ebe0c044ca6a174b51
Rafael Julio <hello.rjulio@gmail.com>
2020-01-23 14:27:31 -0300

    feature(tasks): log service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/auth/user.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -9,6 +14,8 @@ import { TaskRepository } from './task.repository';
 
 @Injectable()
 export class TasksService {
+  private logger = new Logger('TasksService');
+
   constructor(
     @InjectRepository(TaskRepository)
     private taskRepository: TaskRepository
@@ -22,6 +29,9 @@ export class TasksService {
     const found = await this.taskRepository.findOne({});
 
     if (!found) {
+      this.logger.error(
+        `User ${user.username} tried to retrieve Task with ID: ${id}, but no task matching the criteria exists on the server`
+      );
       throw new NotFoundException(
         `Task with ID '${id}' doesn't exist on the server.`
       );
@@ -41,14 +51,26 @@ export class TasksService {
   ): Promise<Task> {
     const task = await this.getTaskById(id, user);
     task.status = status;
-    await task.save();
-    return task;
+
+    try {
+      await task.save();
+      return task;
+    } catch (error) {
+      this.logger.error(
+        'Failed to update task status, see the stacktrace below for more details',
+        error.stack
+      );
+      throw new InternalServerErrorException();
+    }
   }
 
   async deleteTask(id: number, user: User): Promise<void> {
     const result = await this.taskRepository.delete({ id, userId: user.id });
 
     if (result.affected === 0) {
+      this.logger.error(
+        `User ${user.username} tried to delete Task with ID: ${id}, but no task matching the criteria exists on the server`
+      );
       throw new NotFoundException(
         `Task with ID '${id}' doesn't exist on the server.`
       );
