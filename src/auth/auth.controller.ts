32296d05a926ecb7bcd5cfad7f23cc6282aa4205
Rafael Julio <hello.rjulio@gmail.com>
2020-01-23 14:05:29 -0300

    feature(auth): log controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,23 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Logger, Post, ValidationPipe } from '@nestjs/common';
+import * as omit from 'lodash.omit';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Controller('auth')
 export class AuthController {
+  private logger = new Logger('AuthController');
+
   constructor(private authService: AuthService) {}
 
   @Post('/signup')
   signUp(
     @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto
   ): Promise<void> {
+    this.logger.verbose(
+      `Creating a new User with params: ${JSON.stringify(
+        omit(authCredentialsDto, 'password')
+      )}`
+    );
     return this.authService.signUp(authCredentialsDto);
   }
 
@@ -17,6 +25,9 @@ export class AuthController {
   signIn(
     @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto
   ): Promise<{ accessToken: string }> {
+    this.logger.verbose(
+      `Signing-in User: ${JSON.stringify(omit(authCredentialsDto, 'password'))}`
+    );
     return this.authService.signIn(authCredentialsDto);
   }
 }
