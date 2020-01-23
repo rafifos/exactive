b7b90809267e1873e430c78e88ca262bc9ad1dfa
Rafael Julio <hello.rjulio@gmail.com>
2020-01-23 13:49:20 -0300

    feature(tasks): log controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller,
   Delete,
   Get,
+  Logger,
   Param,
   ParseIntPipe,
   Patch,
@@ -25,6 +26,8 @@ import { TasksService } from './tasks.service';
 @Controller('tasks')
 @UseGuards(AuthGuard())
 export class TasksController {
+  private logger = new Logger('TasksController');
+
   constructor(private tasksService: TasksService) {}
 
   @Get()
@@ -32,6 +35,11 @@ export class TasksController {
     @Query(ValidationPipe) filterDto: GetTasksFilterDto,
     @GetUser() user: User
   ): Promise<Task[]> {
+    this.logger.verbose(
+      `User ${user.username} retireving all tasks. Filters: ${JSON.stringify(
+        filterDto
+      )}`
+    );
     return this.tasksService.getTasks(filterDto, user);
   }
 
@@ -40,6 +48,7 @@ export class TasksController {
     @Param('id', ParseIntPipe) id: number,
     @GetUser() user: User
   ): Promise<Task> {
+    this.logger.verbose(`User ${user.username} retrieving task with ID: ${id}`);
     return this.tasksService.getTaskById(id, user);
   }
 
@@ -49,6 +58,11 @@ export class TasksController {
     @Body() createTaskDto: CreateTaskDto,
     @GetUser() user: User
   ): Promise<Task> {
+    this.logger.verbose(
+      `User ${user.username} creating task. Params: ${JSON.stringify(
+        createTaskDto
+      )}`
+    );
     return this.tasksService.createTask(createTaskDto, user);
   }
 
@@ -58,6 +72,9 @@ export class TasksController {
     @Body('status', TaskStatusValidationPipe) status: TaskStatus,
     @GetUser() user: User
   ): Promise<Task> {
+    this.logger.verbose(
+      `User ${user.username} updating task with ID: ${id}. New status is: ${status}`
+    );
     return this.tasksService.updateTaskStatus(id, status, user);
   }
 
@@ -66,6 +83,7 @@ export class TasksController {
     @Param('id', ParseIntPipe) id: number,
     @GetUser() user: User
   ): Promise<void> {
+    this.logger.verbose(`User ${user.username} deleting task with ID: ${id}.`);
     return this.tasksService.deleteTask(id, user);
   }
 }
